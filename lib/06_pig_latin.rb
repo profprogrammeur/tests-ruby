1fe4fbeda8e306bfc7c0faa749d8423969ff2091
profprogrammeur <contact@profprogrammeur.com>
2021-10-11 23:40:17 +0200

    06 finished

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,14 +1,35 @@
-def translate(w)
-  if w[0].count('aeiouy')==1
+ 
+def translate(words)
+words = words.split.map!{|w|
+  if w[0].count('aeiouy')==1  #if begin with voy ad "ay"
     then 
     w+"ay"
   elsif
-    w[0].count('aeiouy')==0 && w[1].count('aeiouy')!=0
-    "#{w.reverse}ay"
-  else
-     w[0,2].count('aeiouy')==0
+    w[0].count('aeiouy')==0 && w[1].count('aeiouy')!=0 #begin with voy and cons
+    "#{w[1,w.length]}#{w[0]}ay"
+  elsif
+     w[0,1].count('aeiouy')==0 #2cons
     "#{w[2,w.length]}#{w[0,2]}ay"
-    
-    # "ananabay"
-  end
+  elsif
+    w[0,2].count('aeiouy')==0 #3 cons
+    "#{w[w.length-2,w.length]}#{w[0,3]}ay"
+  
+  end}.join(" ")
+
 end
+
+
+# def vow(w)
+#  w.split.map!{|i| if i[0].count('aeiouy')==1 then 
+#     i+"ay" else i end}.join(" ")
+#  end
+
+# def vow(i)
+#  if i[0].count('aeiouy')==1 then 
+#     i+"ay" else i end
+#  end
+
+# def cons(i)
+#   if i[0].count('aeiouy')==0 && i[1].count('aeiouy')!=0
+#    then  "#{i.reverse}ay" else i end 
+# end
--- lib/test.rb
+++ lib/test.rb
@@ -10,9 +10,20 @@
 # end
 # repeat("hello")
 
-def titleize(w)
-  w1=w.split
-  w1.map!{|a|a.capitalize}
-  return print  w1
+# def titleize(w)
+#   w1=w.split
+#   w1.map!{|a|a.capitalize}
+#   return print  w1
+# end
+# titleize("abc efg")
+# def vow(w)
+#  w.split.map!{|i| if i[0].count('aeiouy')==1 then 
+#     i+"ay" else i end}.join(" ")
+#  end
+# print vow("aerty banane ertyp")
+def aa(w)
+ w[0].count('aeiouy')==0 && w[1].count('aeiouy')!=0
+    "#{w.reverse}ay"
+
 end
-titleize("abc efg")
+puts aa("pie")
